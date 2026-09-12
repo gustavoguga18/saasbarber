496c6bfac1c7b2d78c435a963235eecd146cb277
GUSTAVO BARBOSA <142503561+gustavoguga18@users.noreply.github.com>
2026-09-12 16:09:08 -0300

    Enhance RootLayout metadata with manifest and themeColor
    Added manifest and themeColor to metadata.

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,9 +4,15 @@ import { establishment } from "@/config/establishment";
 export const metadata = {
   title: establishment.name,
   description: establishment.slogan,
+  manifest: "/manifest.json",
+  themeColor: "#000000",
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="pt-BR">
       <body>{children}</body>
