340a6c12bc1fadb5b1f9431050d6c36da2801ebd
GUSTAVO BARBOSA <142503561+gustavoguga18@users.noreply.github.com>
2026-09-14 08:48:04 -0300

    Change logo file format from JPEG to PNG

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import { createAdminClient } from "@/lib/admin";
-import logo from "./yago-logo.jpeg";
+import logo from "./yago-logo.png";
 
 export const dynamic = "force-dynamic";
 
